5737195c1479baed97759a09544d6e7914b73b5a
Ravi Raju <reachraviraju@gmail.com>
2026-03-18 01:21:16 +0530

    created customErrorcontroller and code modifications in other classes

---
+++ src/main/java/com/ammapickles/backend/repository/OtpVerificationRepository.java
@@ -0,0 +1,12 @@
+package com.ammapickles.backend.repository;
+
+import com.ammapickles.backend.entity.OtpVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface OtpVerificationRepository extends JpaRepository<OtpVerification, Long> {
+    Optional<OtpVerification> findByEmail(String email);
+    void deleteByEmail(String email);
+}
